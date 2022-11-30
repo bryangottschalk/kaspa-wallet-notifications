901a73ca79aa91ec191b6d2811e25099095d6106
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2022-11-30 10:34:53 -0500

    basic error handling

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,6 @@ const KASPA_WALLET_ADDRESS = process.env.KASPA_WALLET_ADDRESS;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const DISCORD_CHANNEL_ID: any = process.env.DISCORD_CHANNEL_ID;
 
-let cachedBalanceInKAS = 0;
-
 // Create the bot
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -27,27 +25,35 @@ client.on('ready', (client) => {
   channel = client.channels.cache.get(DISCORD_CHANNEL_ID);
 });
 
+let cachedBalanceInKAS = 0;
+
 setInterval(async () => {
   console.log('querying wallet...');
 
-  const {
-    data: { balance }
-  } = await axios.get(
-    `https://api.kaspa.org/addresses/${KASPA_WALLET_ADDRESS}/balance`
-  );
-  const currentBalanceInKAS = balance / 100000000;
-  const CURRENT_BALANCE_MSG = `Current KAS balance: ${currentBalanceInKAS}`;
-  console.log(CURRENT_BALANCE_MSG);
+  try {
+    const {
+      data: { balance }
+    } = await axios.get(
+      `https://api.kaspa.org/addresses/${KASPA_WALLET_ADDRESS}/balance`
+    );
+    const currentBalanceInKAS = balance / 100000000;
+    const CURRENT_BALANCE_MSG = `Current KAS balance: ${currentBalanceInKAS}`;
+    console.log(CURRENT_BALANCE_MSG);
 
-  if (cachedBalanceInKAS && cachedBalanceInKAS < currentBalanceInKAS) {
-    const differenceInKAS = currentBalanceInKAS - cachedBalanceInKAS;
-    const TRANSACTION_OCCURED_MSG = `Transaction occurred! Wallet balance modified by a difference in ${differenceInKAS} KAS.`;
-    console.log(TRANSACTION_OCCURED_MSG);
+    if (cachedBalanceInKAS && cachedBalanceInKAS < currentBalanceInKAS) {
+      const differenceInKAS = currentBalanceInKAS - cachedBalanceInKAS;
+      const TRANSACTION_OCCURED_MSG = `Transaction occurred! Wallet balance modified by a difference in ${differenceInKAS} KAS.`;
+      console.log(TRANSACTION_OCCURED_MSG);
 
-    channel.send(TRANSACTION_OCCURED_MSG);
-    channel.send(CURRENT_BALANCE_MSG);
+      channel.send(TRANSACTION_OCCURED_MSG);
+      channel.send(CURRENT_BALANCE_MSG);
+    }
+    cachedBalanceInKAS = currentBalanceInKAS;
+  } catch (err) {
+    const ERROR_MSG = `Error occured when querying wallet address: ${err}`;
+    console.log(ERROR_MSG);
+    channel.send(ERROR_MSG);
   }
-  cachedBalanceInKAS = currentBalanceInKAS;
 }, TEN_SECONDS);
 
 app.get('/', (req: Request, res: Response) => {
