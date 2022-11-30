3cce79c61a2faf14aae070e0856f88fab27037cd
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2022-11-30 00:59:03 -0500

    add axios; query wallet

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+const TEN_SECONDS = 10000;
+const WALLET_ADDRESS =
+  'kaspa%3Aqrkxtuedt9ru9kdwgpjvusp64upkcyt8vpmw7k52zj0qnsjj3qmsxj4r0grwx';
+let cachedBalanceInKAS = 0;
+
+setInterval(async () => {
+  console.log('querying wallet...');
+
+  const {
+    data: { balance }
+  } = await axios.get(
+    `https://api.kaspa.org/addresses/${WALLET_ADDRESS}/balance`
+  );
+  const currentBalanceInKAS = balance / 100000000;
+  console.log(`current KAS balance: ${currentBalanceInKAS}`);
+
+  if (cachedBalanceInKAS && cachedBalanceInKAS < currentBalanceInKAS) {
+    const differenceInKAS = currentBalanceInKAS - cachedBalanceInKAS;
+    console.log(
+      `transaction occurred! wallet balance modified by a difference in ${differenceInKAS} KAS.`
+    );
+  }
+  cachedBalanceInKAS = currentBalanceInKAS;
+}, TEN_SECONDS);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Listening for your wallet address...');
