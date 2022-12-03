567eff5bc3b95f0d88e7b25e8e35d54797d7488d
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2022-12-02 21:45:28 -0500

    begin polling without delay

--- src/poll-wallet-address.ts
+++ src/poll-wallet-address.ts
@@ -2,60 +2,55 @@ import axios, { AxiosResponse } from 'axios';
 import { sendDiscordChannelMessage } from './helpers';
 import { TextChannel } from 'discord.js';
 
+const KASPA_PUBLIC_API = 'https://api.kaspa.org';
 const FIVE_MINUTES = 300000;
 const KASPA_WALLET_ADDRESS = process.env.KASPA_WALLET_ADDRESS;
 
-const queryForTransaction = async (channel: TextChannel) => {
-  let cachedBalanceInKAS: number = 0;
-  console.log('querying wallet...');
-
-  try {
-    const {
-      data: { balance }
-    }: AxiosResponse<{ balance: number }> = await axios.get(
-      `https://api.kaspa.org/addresses/${KASPA_WALLET_ADDRESS}/balance`
-    );
-
-    const { data: blockReward }: AxiosResponse<string> = await axios.get(
-      `https://api.kaspa.org/info/blockreward?stringOnly=true`
-    );
-    const roundedBlockReward: number = Math.round(Number(blockReward));
-
-    const currentBalanceInKAS = balance / 100000000;
-    const CURRENT_BALANCE_MSG = `Current KAS balance: ${currentBalanceInKAS}`;
-    console.log(CURRENT_BALANCE_MSG);
-
-    if (
-      cachedBalanceInKAS !== 0 &&
-      cachedBalanceInKAS !== currentBalanceInKAS
-    ) {
-      const differenceInKAS = currentBalanceInKAS - cachedBalanceInKAS;
-      const roundedDifferenceInKAS = Math.round(differenceInKAS);
-      const TRANSACTION_OCCURED_MSG = `${
-        roundedDifferenceInKAS === roundedBlockReward
-          ? 'Block mined!'
-          : 'Transaction occurred!'
-      }  Wallet balance modified by a difference in ${differenceInKAS} KAS.`;
-      console.log(TRANSACTION_OCCURED_MSG);
-
-      sendDiscordChannelMessage(channel, TRANSACTION_OCCURED_MSG);
-      sendDiscordChannelMessage(channel, CURRENT_BALANCE_MSG);
-    }
-    cachedBalanceInKAS = currentBalanceInKAS;
-  } catch (err) {
-    const ERROR_MSG = `Error occured when querying wallet address: ${err}`;
-    console.log(ERROR_MSG);
-  }
+const setIntervalImmediately = (func: Function, interval: number) => {
+  func();
+  return setInterval(func, interval);
 };
 
-export const pollWalletAddress = (channel: TextChannel): void => {
-  queryForTransaction(channel);
-  console.log(
-    `checking wallet for transactions every ${Math.floor(
-      FIVE_MINUTES / 1000 / 60
-    )} minutes...`
-  );
-  setInterval(async () => {
-    queryForTransaction(channel);
+export const pollWalletAddress = async (channel: TextChannel) => {
+  let cachedBalanceInKAS: number = 0;
+  setIntervalImmediately(async () => {
+    try {
+      console.log('querying wallet...');
+      const {
+        data: { balance }
+      }: AxiosResponse<{ balance: number }> = await axios.get(
+        `${KASPA_PUBLIC_API}/addresses/${KASPA_WALLET_ADDRESS}/balance`
+      );
+
+      const { data: blockReward }: AxiosResponse<string> = await axios.get(
+        `${KASPA_PUBLIC_API}/info/blockreward?stringOnly=true`
+      );
+      const roundedBlockReward: number = Math.round(Number(blockReward));
+
+      const currentBalanceInKAS = balance / 100000000;
+      const CURRENT_BALANCE_MSG = `Current KAS balance: ${currentBalanceInKAS}`;
+      console.log(CURRENT_BALANCE_MSG);
+
+      if (
+        cachedBalanceInKAS !== 0 &&
+        cachedBalanceInKAS !== currentBalanceInKAS
+      ) {
+        const differenceInKAS = currentBalanceInKAS - cachedBalanceInKAS;
+        const roundedDifferenceInKAS = Math.round(differenceInKAS);
+        const TRANSACTION_OCCURED_MSG = `${
+          roundedDifferenceInKAS === roundedBlockReward
+            ? 'Block mined!'
+            : 'Transaction occurred!'
+        }  Wallet balance modified by a difference in ${differenceInKAS} KAS.`;
+        console.log(TRANSACTION_OCCURED_MSG);
+
+        sendDiscordChannelMessage(channel, TRANSACTION_OCCURED_MSG);
+        sendDiscordChannelMessage(channel, CURRENT_BALANCE_MSG);
+      }
+      cachedBalanceInKAS = currentBalanceInKAS;
+    } catch (err) {
+      const ERROR_MSG = `Error occured when querying wallet address: ${err}`;
+      console.log(ERROR_MSG);
+    }
   }, FIVE_MINUTES);
 };
